2ad62433d1158e77d8063aa9fa5258c6f9dd536e
tubbdoose <samuel.m.d.low@gmail.com>
2022-08-03 22:38:48 -0600

    Finished basic setup and created essential functions (dialogue and animation)

---
+++ classes.py
@@ -0,0 +1,177 @@
+import sys
+import pygame
+from pathlib import Path
+from pygame.locals import *
+
+colour = {
+    "black": (0, 0, 0),
+    "white": (255, 255, 255),
+    "red": (255, 156, 160),
+    "blue": (152, 233, 235),
+    "green": (203, 251, 164),
+    "orange": (245, 215, 157),
+    "purple": (190, 162, 250)
+}
+
+class Settings():
+    def __init__(self, windowName="Game", icon="images/characters/Low/happy/happy1.png", framerate=60):
+        self.monitor_ratio = pygame.display.get_desktop_sizes()[0][0] / pygame.display.get_desktop_sizes()[0][1]
+        pygame.display.set_caption(windowName)
+        pygame.display.set_icon(pygame.image.load(icon))
+        self.framerate = framerate
+
+class Sprite():
+    def __init__(self, image, x, y, width=None, height=None):
+        self.image = image
+        self.screen = pygame.image.load(image)
+        self.ratio = self.screen.get_width()/self.screen.get_height()
+        self.x = x
+        self.y = y
+
+        # animation
+        self.animation = []
+        self.frame = 0
+        self.frameGap = 3
+        self.frameCount = 0
+        self.looping = True
+
+        if width != None:
+            self.setWidth(width)
+        if height != None:
+            self.setHeight(height)
+
+    def loadAnimation(self, file, looping=True):
+        # images/characters/Low/happy
+        self.looping = looping
+        p = Path(file).glob('**/*')
+        self.animation = [x for x in p if x.is_file()]
+        self.frameCount = 0
+
+    def setWidth(self, width):
+        self.width = width
+        self.height = (width/self.ratio) * settings.monitor_ratio
+        self.screen = pygame.transform.smoothscale(pygame.image.load(self.image), (calcWidth(self.width), calcHeight(self.height)))
+
+    def setHeight(self, height):
+        self.width = (height*self.ratio) / settings.monitor_ratio
+        self.height = height
+        self.screen = pygame.transform.smoothscale(pygame.image.load(self.image), (calcWidth(self.width), calcHeight(self.height)))
+
+    def centerX(self):
+        self.x = (1 - self.width)/2
+
+    def centerY(self):
+        self.y = (1 - self.height)/2
+
+    def setImage(self, image):
+        self.image = image
+        self.screen = pygame.image.load(image)
+        self.ratio = self.screen.get_width()/self.screen.get_height()
+
+    def update(self):
+        if len(self.animation) > 0:
+            self.screen = pygame.transform.smoothscale(pygame.image.load(self.animation[self.frame]), (calcWidth(self.width), calcHeight(self.height)))
+        else:
+            self.screen = pygame.transform.smoothscale(pygame.image.load(self.image),(calcWidth(self.width), calcHeight(self.height)))
+
+        if self.frameCount != None:
+            self.frameCount += 1
+
+            if self.frameCount >= self.frameGap:
+                self.frameCount = 0
+                self.frame += 1
+
+            if self.frame == len(self.animation):
+                if self.looping == True:
+                    self.frame = 0
+                else:
+                    self.frame -= 1
+                    self.frameCount = None
+
+    def stamp(self, surface):
+        surface.blit(self.screen, (calcWidth(self.x), calcHeight(self.y)))
+
+class TextBox(Sprite):
+    def __init__(self, image, x, y, text="", textSize=0.025, textMargin=0.03, lineSpacing=1.5, font="freesansbold.ttf", width=None, height=None):
+        super().__init__(image, x, y, width=width, height=height)
+        self.textSize = textSize
+        self.textMargin = textMargin
+        self.lineSpacing = lineSpacing
+        self.fontName = font
+        self.font = pygame.font.Font(font, int(calcHeight(textSize)))
+        self.text = text
+
+    def getLines(self):
+        self.font = pygame.font.Font(self.fontName, int(calcHeight(self.textSize)))
+        lines = []
+        index = 0
+        lastSpace = 0
+        textString = self.text
+        while index < len(textString):
+            if textString[index] == " ":
+                text = self.font.render(textString[0:index], True, colour["black"])
+                if text.get_width() > calcWidth(self.width - (2*self.textMargin)):
+                    lines.append(textString[0:lastSpace])
+                    textString = textString[lastSpace + 1:]
+                    index = 0
+                    lastSpace = 0
+
+                elif text.get_width() < calcWidth(self.width - (2*self.textMargin)):
+                    lastSpace = index
+
+            index += 1
+        lines.append(textString[0:])
+        return lines
+
+    def update(self):
+        self.font = pygame.font.Font(self.fontName, int(calcHeight(self.textSize)))
+        lines = self.getLines()
+        self.screen = pygame.transform.smoothscale(pygame.image.load(self.image), (calcWidth(self.width), calcHeight(self.height)))
+        multi = 0
+        topMargin = center((len(lines)*(self.textSize*self.lineSpacing)) - (self.textSize*(self.lineSpacing-1)), self.height)
+        for line in lines:
+            text = self.font.render(line, True, colour["black"])
+            self.screen.blit(text, (calcWidth(self.textMargin), calcHeight(topMargin) + calcHeight(self.textSize * multi * self.lineSpacing)))
+            multi += 1
+
+def center(item, domain):
+    return (domain - item)/2
+
+def calcHeight(fraction):
+    global screen
+    return screen.get_height() * fraction
+
+def calcWidth(fraction):
+    global screen
+    return screen.get_width() * fraction
+
+def windowCheck(event):
+    if event.type == pygame.QUIT:
+        sys.exit()
+
+    elif event.type == VIDEORESIZE:
+        width, height = event.size
+        if width < size[0]:
+            width = size[0]
+        if height < size[1]:
+            height = size[1]
+
+        if width / height != settings.monitor_ratio:
+            height = width * (1 / settings.monitor_ratio)
+
+        screen = pygame.display.set_mode((width, height), RESIZABLE)
+
+def updateScreen(sprites):
+    screen.fill(colour["black"])
+
+    for sprite in sprites:
+        sprite.update()
+
+    for sprite in sprites:
+        sprite.stamp(screen)
+
+pygame.init()
+
+size = [800, 450]
+screen = pygame.display.set_mode(size, RESIZABLE)
+settings = Settings()
--- engine.py
+++ engine.py
@@ -2,102 +2,49 @@ import sys
 import pygame
 import pathlib
 from pygame.locals import *
-pygame.init()
+from classes import *
 
-colour = {
-    "black": (0, 0, 0),
-    "white": (255, 255, 255),
-    "red": (255, 156, 160),
-    "blue": (152, 233, 235),
-    "green": (203, 251, 164),
-    "orange": (245, 215, 157),
-    "purple": (190, 162, 250)
-}
+background = Sprite("images/backgrounds/bridge.jpg", 0, 0, width=1)
+background.centerX()
 
-monitor_ratio = pygame.display.get_desktop_sizes()[0][0]/pygame.display.get_desktop_sizes()[0][1]
+textbox = TextBox("images/UI/textbox.svg", 0, 0.75, width=0.75)
+textbox.centerX()
 
-size = [450 * monitor_ratio, 450]
-screen = pygame.display.set_mode(size, RESIZABLE)
-pygame.display.set_caption('Game')
-
-def textSize():
-    return int(0.025*screen.get_height())
-
-def center(item, domain):
-    return (domain - item)/2
-
-def calcHeight(fraction):
-    global screen
-    return screen.get_height() * fraction
-
-def calcWidth(fraction):
-    global screen
-    return screen.get_width() * fraction
-
-font = pygame.font.Font('freesansbold.ttf', textSize())
-text = font.render('GeeksForGeeks', True, colour["black"])
-textRect = text.get_rect()
-
-background = pygame.image.load("images/backgrounds/bridge.jpg")
-textbox = pygame.image.load("images/UI/textbox.png")
-character = pygame.image.load("images/characters/Low/Low_neutral.png")
-character_ratio = character.get_width()/character.get_height()
+character = Sprite("images/characters/Low/happy/happy1.png", 0.3, 0.15, height=1.5)
 # HWSURFACE | DOUBLEBUF
+character.loadAnimation("images/characters/Low/happy")
 
-textString = 'Hello my name is sam low. I love going to school and learning things. My favourite subject at school is math. I love to math!'
+textbox.text = 'Hello my name is sam low. I love going to school and learning things. My favourite subject at school is math. I love to math!'
 
-lines = []
-index = 0
-lastSpace = 0
-while index < len(textString):
-    if textString[index] == " ":
-        text = font.render(textString[0:index], True, colour["black"])
-        if text.get_width() > screen.get_width() * 0.9:
-            lines.append(textString[0:lastSpace])
-            textString = textString[lastSpace+1:]
-            index = 0
-            lastSpace = 0
+clock = pygame.time.Clock()
 
-        elif text.get_width() < screen.get_width() * 0.9:
-            lastSpace = index
+def dialogue(text="...", characterName=None, animation=None):
+    finished = False
 
-    index += 1
-lines.append(textString[0:])
+    if animation != None:
+        character.loadAnimation("images/characters/"+characterName + "/" + animation)
+    textbox.text = text
+
+    sprites = [background]
+    if characterName != None:
+        sprites.append(character)
+    sprites.append(textbox)
+
+    while not finished:
+        clock.tick(settings.framerate)
+        for event in pygame.event.get():
+            windowCheck(event)
+
+        updateScreen(sprites)
+        if True in pygame.key.get_pressed():
+            finished = True
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        dialogue(text="I love doing it", characterName="Low", animation="happy")
+        dialogue(text="By it I mean sex", characterName="Low", animation="happy")
+        dialogue(text="I love sex", characterName="Low", animation="happy")
 
-            elif event.type == VIDEORESIZE:
-                width, height = event.size
-                if width < size[0]:
-                    width = size[0]
-                if height < size[1]:
-                    height = size[1]
-
-                if width/height != monitor_ratio:
-                    height = width * (1/monitor_ratio)
-
-                screen = pygame.display.set_mode((width, height), RESIZABLE)
-                #pygame.display.flip()
-
-        screen.fill(colour["black"])
-        background = pygame.transform.smoothscale(background, screen.get_size())
-        textbox = pygame.transform.smoothscale(textbox, (calcWidth(0.9), calcHeight(0.2)))
-        character = pygame.transform.smoothscale(character, (calcHeight(1.5)*character_ratio, calcHeight(1.5)))
-        font = pygame.font.Font('freesansbold.ttf', textSize())
-
-        screen.blit(background, (0, 0))
-        screen.blit(character, (calcWidth(0.3), calcHeight(0.15)))
-        screen.blit(textbox, (center(textbox.get_width(), screen.get_width()), calcHeight(0.75)))
-
-        multi = 0
-        for line in lines:
-            text = font.render(line, True, colour["black"])
-            screen.blit(text, (center(textbox.get_width(), screen.get_width()) + calcWidth(0.02), (calcHeight(0.79) + (multi * textSize() * 1.01))))
-            multi += 1
-
-        pygame.display.flip()
